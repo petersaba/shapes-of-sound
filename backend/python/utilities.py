adb64889da9718483285190b80f8a92b331c6c16
Peter Saba <petersaba001@gmail.com>
2022-11-02 20:16:08 +0200

    asr <> feature embedding: adding convolutional layers to extract speech features

--- backend/python/utilities.py
+++ backend/python/utilities.py
@@ -32,6 +32,15 @@ def addEmbeddings(words, vocabulary_size=len(vocabulary), max_sentence_length=MA
 
     return word_embedding + positional_embedding
 
+# extract features to be fed to the encoder
+def applySpeechFeatureEmbedding(kernel_num):
+    conv_layer1 = keras.layers.Conv1D(kernel_num, 11, strides=2, activation='relu')
+    conv_layer2 = keras.layers.Conv1D(kernel_num, 11, strides=2, activation='relu')
+    conv_layer3 = keras.layers.Conv1D(kernel_num, 11, strides=2, activation='relu')
+
+
+
+
 if __name__ ==  "__main__":
 
     # a = tf.constant([
