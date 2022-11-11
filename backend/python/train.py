7a2dd9e1f122cd3f155fc7c8232a2931de6c4fd5
Peter Saba <petersaba001@gmail.com>
2022-11-12 01:37:13 +0200

    asr <> training: loading data into train and validation arrays

--- backend/python/train.py
+++ backend/python/train.py
@@ -3,3 +3,9 @@ import transformer
 import utilities
 keras = tf.keras
 
+if __name__ == '__main__':
+    data = utilities.getAudioTranscriptions()
+    validation_split = int(len(data) * 0.99)
+    
+    train_data = data[: validation_split]
+    validation_data = [validation_split :]
