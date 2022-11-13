17c732173dbbae9175fca9a8b8becd9725b961a1
Peter Saba <petersaba001@gmail.com>
2022-11-13 03:38:21 +0200

    asr <> training: compiling and training the model

--- backend/python/train.py
+++ backend/python/train.py
@@ -6,18 +6,21 @@ keras = tf.keras
 if __name__ == '__main__':
     data = utilities.getAudioTranscriptions()
     validation_split = int(len(data) * 0.99)
-    
     train_data = data[: validation_split]
     validation_data = data[validation_split :]
 
-    train_dataset = utilities.createFullDataset(train_data, 64)
+
+    train_dataset = utilities.createFullDataset(train_data, 32)
     validation_dataset = utilities.createFullDataset(validation_data)
 
     test_batch = next(iter(validation_dataset)) # batch to be used when testing once every 5 epochs
     display_test = transformer.DisplayOutputs(test_batch)
 
-    model = transformer.Transformer(key_dimension=200, target_maxlen=200)
+    model = transformer.Transformer()
     loss = keras.losses.CategoricalCrossentropy(label_smoothing=0.1, from_logits=True)
 
     learning_rate = transformer.CustomSchedule(steps_per_epoch=len(train_dataset))
     optimizer = keras.optimizers.Adam(learning_rate)
+    model.compile(optimizer, loss=loss)
+
+    model.fit(train_dataset, validation_data=validation_dataset, callbacks=[display_test], epochs=100)
