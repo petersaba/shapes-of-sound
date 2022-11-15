1afdddc7160b01164d4a6cd0137775322673759c
Peter Saba <petersaba001@gmail.com>
2022-11-16 00:37:38 +0200

    frontend <> general: creating build function for custom app bar

--- frontend/lib/widgets/appBars.dart
+++ frontend/lib/widgets/appBars.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title});
-
-  final title;
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppBar(title: const Text('SHAPES OF SOUND'));
   }
 }
