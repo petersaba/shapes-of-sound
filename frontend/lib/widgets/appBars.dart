ffee5354727e5a7d9dc2749a173f1787619e7151
Peter Saba <petersaba001@gmail.com>
2022-11-15 23:17:58 +0200

    frontend <> general: creating custom app bar widget

---
+++ frontend/lib/widgets/appBars.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key, required this.title});
+
+  final title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
