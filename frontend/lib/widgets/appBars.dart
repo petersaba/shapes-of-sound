c0032443940df526cbb6646cb931a70b0f2e461d
Peter Saba <petersaba001@gmail.com>
2022-11-16 15:07:01 +0200

    frontend <> general: styling custom app bar

--- frontend/lib/widgets/appBars.dart
+++ frontend/lib/widgets/appBars.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: file_names
+import 'package:flutter/material.dart'; // could not fix warning for naming of file
 
-class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
+  Size get preferredSize => const Size.fromHeight(80);
+
+  @override
   Widget build(BuildContext context) {
-    return AppBar(title: const Text('SHAPES OF SOUND'));
+    return AppBar(
+      flexibleSpace: Column(children: <Widget>[
+        const SizedBox(height: 55),
+        Row(
+          children: const <Widget>[
+            SizedBox(width: 20,),
+            Text(
+              'Shapes Of Sound',
+              style: TextStyle(
+                fontFamily: 'AlfaSlabOne', 
+                fontSize: 22,
+                color: Colors.white),
+            ),
+          ]
+        )
+      ]),
+    );
   }
 }
