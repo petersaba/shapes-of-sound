2a8e504bcb06483c18f316df9bb02fac8d431eee
Peter Saba <petersaba001@gmail.com>
2022-11-17 01:00:17 +0200

    frontend <> general: adding widget for main page body

---
+++ frontend/lib/widgets/translate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomepageMainSection extends StatefulWidget {
+  const HomepageMainSection({super.key});
+
+  @override
+  State<HomepageMainSection> createState() => _HomepageMainSectionState();
+}
+
+class _HomepageMainSectionState extends State<HomepageMainSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
