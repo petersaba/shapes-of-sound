f2133c23010700c5466092cca240fa9cfa4cdfb7
Peter Saba <petersaba001@gmail.com>
2022-11-17 02:59:22 +0200

    frontend <> general: adding sized box for record button in homepage main section

--- frontend/lib/widgets/translate.dart
+++ frontend/lib/widgets/translate.dart
@@ -23,7 +23,7 @@ class _HomepageMainSectionState extends State<HomepageMainSection> {
           height: 60,
           color: const Color(0x4D808080),
           child: Row(
-            children: [
+            children: const [
               Expanded(
                   child: TextField(
                 style: TextStyle(fontSize: 18),
@@ -31,7 +31,14 @@ class _HomepageMainSectionState extends State<HomepageMainSection> {
                     border: InputBorder.none,
                     hintText: 'Type here',
                     contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0)),
-              ))
+              )),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                child: SizedBox(
+                  width: 40,
+                  height: 40,
+                ),
+              )
             ],
           ),
         )
