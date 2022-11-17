459e61f9117d6e350c89774e5dff0db77c8bfc85
Peter Saba <petersaba001@gmail.com>
2022-11-17 23:36:36 +0200

    frontend <> general: adding widget for user text user input

---
+++ frontend/lib/widgets/text_input.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TextInput extends StatefulWidget {
+  const TextInput({super.key});
+
+  @override
+  State<TextInput> createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        
+      ],
+    );
+  }
+}
