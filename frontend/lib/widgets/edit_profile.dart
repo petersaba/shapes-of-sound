36f2f263979c4a375762f03aaad9351a00a5d937
Peter Saba <petersaba001@gmail.com>
2022-11-17 22:58:19 +0200

    frontend <> edit profile: changing layout to list view

--- frontend/lib/widgets/edit_profile.dart
+++ frontend/lib/widgets/edit_profile.dart
@@ -14,37 +14,37 @@ class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: double.infinity,
       width: double.infinity,
       color: const Color(0xFFF3F5F8),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 20, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => 10,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFFF0000),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                      minimumSize: const Size(100, 43)
-                    ),
-                    child: const Text('LOGOUT', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                  )
-                ],
-              ),
-            ),
-            Image.asset(
-              'assets/images/no-profile.png',
-              width: 226,
-              height: 226,
-              fit: BoxFit.cover,
-            ),
-          ]),
+      child: ListView(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 20, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () => 10,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFF0000),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    minimumSize: const Size(100, 43)),
+                child: const Text(
+                  'LOGOUT',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          'assets/images/no-profile.png',
+          width: 226,
+          height: 226,
+          fit: BoxFit.cover,
+        ),
+      ]),
     );
   }
 }
