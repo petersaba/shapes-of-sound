3118b954d2003d305c98b90a43b5e99b35cc090d
Peter Saba <petersaba001@gmail.com>
2022-11-17 16:09:07 +0200

    frontend <> general: renaming appBars file to app_bars

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/widgets/appBars.dart';
+import 'package:frontend/widgets/app_bars.dart';
 import 'utilities.dart';
 import 'package:frontend/widgets/translate.dart';
 
