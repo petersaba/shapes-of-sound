63d03047019278b238955f32f232d19dcbc64e18
Peter Saba <petersaba001@gmail.com>
2022-11-18 01:41:20 +0200

    frontend <> general: deciding width when using form button

--- frontend/lib/widgets/form_button.dart
+++ frontend/lib/widgets/form_button.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
 class FormButton extends StatelessWidget {
-  const FormButton({super.key});
+  const FormButton({super.key, required this.width});
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          ElevatedButton(
-            onPressed: () => 10,
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(110, 47),
-                backgroundColor: const Color(0xFF28AFB0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15))),
-            child: const Text(
-              'Edit image',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-            ),
-          )
-        ]);
+      ElevatedButton(
+        onPressed: () => 10,
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size(width, 47),
+            backgroundColor: const Color(0xFF28AFB0),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15))),
+        child: const Text(
+          'Edit image',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+      )
+    ]);
   }
 }
