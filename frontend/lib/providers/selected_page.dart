b38a85da0eaf8b5597da299aa7bcf52bf6674068
Peter Saba <petersaba001@gmail.com>
2022-11-17 20:39:11 +0200

    frontend <> general: adding setter for selected page

--- frontend/lib/providers/selected_page.dart
+++ frontend/lib/providers/selected_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-
 class SelectedPage with ChangeNotifier {
   int _selectedPage = 0;
 
   int get selectedPage => _selectedPage;
+
+  set selectedPage(int value) {
+    _selectedPage = value;
+    notifyListeners();
+  }
 }
