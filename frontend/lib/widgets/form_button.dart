6f1395d86456a0c8475f2961db59db0777da10c2
Peter Saba <petersaba001@gmail.com>
2022-11-18 00:48:28 +0200

    frontend <> general: styling form button

--- frontend/lib/widgets/form_button.dart
+++ frontend/lib/widgets/form_button.dart
@@ -1,11 +1,23 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class FormButton extends StatelessWidget {
   const FormButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+            onPressed: () => 10,
+            style: ElevatedButton.styleFrom(
+                minimumSize: const Size(110, 47),
+                backgroundColor: const Color(0xFF28AFB0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            child: const Text(
+              'Edit image',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            ),
+          )
+        ]);
   }
 }
