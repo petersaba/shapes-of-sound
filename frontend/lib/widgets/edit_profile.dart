e77093287d0d143aff741c82d6baa36e4fd31407
Peter Saba <petersaba001@gmail.com>
2022-11-17 21:36:59 +0200

    frontend <> edit profile: returning container for main section

---
+++ frontend/lib/widgets/edit_profile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: const Color(0xFFF3F5F8),
+      
+    );
+  }
+}
