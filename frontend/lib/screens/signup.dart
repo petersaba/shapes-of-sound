4e975d81f1180a29d5d1bd6722f677b5d367706c
Peter Saba <petersaba001@gmail.com>
2022-11-18 20:42:34 +0200

    frontend <> signup: adding add image button

--- frontend/lib/screens/signup.dart
+++ frontend/lib/screens/signup.dart
@@ -1,8 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:frontend/widgets/form_button.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -16,19 +13,22 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
+          height: double.infinity,
             color: const Color(0xFFF3F5F8),
             child: ListView(
               shrinkWrap: true,
-              children: const [
-                Center(
+              children: [
+                const Center(
                     child: Text(
                   'Sign Up',
                   style: TextStyle(fontFamily: 'AlfaSlabOne', fontSize: 45),
                 )),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                // Image.asset('assets/images/no-profile.png', width: 200, height: 200,)
+                Image.asset('assets/images/no-profile.png', width: 200, height: 200,),
+                const SizedBox(height: 20,),
+                const FormButton(width: 110, text: 'Add image')
               ],
             )));
   }
