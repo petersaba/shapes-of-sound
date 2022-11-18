3a15e07c3ba6ffeea98149a645ef7bc5b664a1e0
Peter Saba <petersaba001@gmail.com>
2022-11-18 10:17:42 +0200

    backend <> routes: adding route to transcribe audio

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TranscribeController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('test', [AuthController::class, 'test']);
+Route::post('transcribe', [TranscribeController::class, 'transcribeAudio']);
