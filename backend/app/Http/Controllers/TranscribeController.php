d0119dbf760a7559dc586d2d59931e4e7a418ecd
Peter Saba <petersaba001@gmail.com>
2022-11-18 10:02:30 +0200

    backend <> transcribe audio: decoding base64 audio file

--- backend/app/Http/Controllers/TranscribeController.php
+++ backend/app/Http/Controllers/TranscribeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class TranscribeController extends Controller
 {
-    //
+    function transcribeAudio(Request $request){
+        $base64_encoded_audio = $request->encoded_audio;
+        $decoded_audio = base64_decode($base64_encoded_audio);
+
+    }
 }
