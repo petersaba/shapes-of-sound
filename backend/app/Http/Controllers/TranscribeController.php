7070b144d01945aafe91b82ac6f9e74b18c5b7b5
Peter Saba <petersaba001@gmail.com>
2022-11-18 18:48:47 +0200

    backend <> transcribe audio: saving audio path in database

--- backend/app/Http/Controllers/TranscribeController.php
+++ backend/app/Http/Controllers/TranscribeController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recording;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TranscribeController extends Controller
 {
     function transcribeAudio(Request $request){
+        $current_date = date('Y-m-d-H-i-s');
+        $user_id = Auth::id();
         $base64_encoded_audio = $request->encoded_audio;
         $decoded_audio = base64_decode($base64_encoded_audio);
 
-        file_put_contents('../python/audio.wav', $decoded_audio);
+        file_put_contents('../python/audios/' . $user_id . $current_date . '.wav', $decoded_audio);
+        $recording = new Recording;
+        $recording->user_id = $user_id;
+        $recording->recording_url = '../python/audios/' . $user_id . $current_date . '.wav';
+
+        if($recording->save()){
+            return response()->json([
+                'success' => TRUE
+            ]);
+        }
     }
 }
