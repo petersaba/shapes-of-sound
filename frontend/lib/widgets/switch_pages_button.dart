c43f52604e659f1a32c7a2eb38ec715196268782
Peter Saba <petersaba001@gmail.com>
2022-11-18 14:15:41 +0200

    frontend <> general: changing text and button layout in switch button widget

--- frontend/lib/widgets/switch_pages_button.dart
+++ frontend/lib/widgets/switch_pages_button.dart
@@ -5,15 +5,29 @@ class SwitchButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: (() => 10),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent
-        ),
-        child: Row(
-          children: const [Text("Don't have an account? ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 16),),
-          Text('Sign Up', style: TextStyle(color: Color(0xFF28AFB0), fontSize: 16, fontWeight: FontWeight.bold))],
-        ));
+    return SizedBox(
+      width: 330,
+        child: ElevatedButton(
+            onPressed: (() => 10),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Don't have an account? ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16),
+                ),
+                Text('Sign Up',
+                    style: TextStyle(
+                        color: Color(0xFF28AFB0),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold))
+              ],
+            )));
   }
 }
