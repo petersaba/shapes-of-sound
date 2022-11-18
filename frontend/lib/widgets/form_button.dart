dac0ebbdac287a562d52fb598bebaebe85fee9d4
Peter Saba <petersaba001@gmail.com>
2022-11-18 23:55:30 +0200

    frontend <> general: adding optional route to form button

--- frontend/lib/widgets/form_button.dart
+++ frontend/lib/widgets/form_button.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class FormButton extends StatelessWidget {
-  const FormButton({super.key, required this.width, required this.text});
+  const FormButton(
+      {super.key, required this.width, required this.text, this.route});
+  final String? route;
   final double width;
   final String text;
+
+  void _navigate(BuildContext context) {
+    if (route != null) {
+      Navigator.pushReplacementNamed(context, route!);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
       ElevatedButton(
-        onPressed: () => 10,
+        onPressed: () => _navigate(context),
         style: ElevatedButton.styleFrom(
             minimumSize: Size(width, 47),
             backgroundColor: const Color(0xFF28AFB0),
