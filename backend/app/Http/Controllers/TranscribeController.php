a00989aca1953f91438c10dc3929abb2e06409fe
Peter Saba <petersaba001@gmail.com>
2022-11-18 10:17:05 +0200

    backend <> transcribe audio: saving audio file

--- backend/app/Http/Controllers/TranscribeController.php
+++ backend/app/Http/Controllers/TranscribeController.php
@@ -10,5 +10,6 @@ class TranscribeController extends Controller
         $base64_encoded_audio = $request->encoded_audio;
         $decoded_audio = base64_decode($base64_encoded_audio);
 
+        file_put_contents('../python/audio.wav', $decoded_audio);
     }
 }
