48510bceb474fbf4e6072edfaf9b8271eb07812c
Peter Saba <petersaba001@gmail.com>
2022-11-18 20:12:13 +0200

    frontend <> signup: cleaning code

--- frontend/lib/screens/signup.dart
+++ frontend/lib/screens/signup.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -16,11 +18,18 @@ class _SignUpPageState extends State<SignUpPage> {
         body: Container(
             color: const Color(0xFFF3F5F8),
             child: ListView(
-              children: const [Center(child: Text(
-                              'Sign Up',
-                              style: TextStyle(
-                                  fontFamily: 'AlfaSlabOne', fontSize: 45),
-                            ))],
+              shrinkWrap: true,
+              children: const [
+                Center(
+                    child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontFamily: 'AlfaSlabOne', fontSize: 45),
+                )),
+                SizedBox(
+                  height: 20,
+                ),
+                // Image.asset('assets/images/no-profile.png', width: 200, height: 200,)
+              ],
             )));
   }
 }
