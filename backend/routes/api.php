be0870b851b83c547d5bd80745cd2ba2f31accfe
Peter Saba <petersaba001@gmail.com>
2022-11-18 17:16:59 +0200

    backend <> routes: naming login route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('signup', [AuthController::class, 'createUser']);
-Route::post('login', [AuthController::class, 'login']);
-Route::group(['Middleware' => 'auth:api'], function(){
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => 'auth:api'], function(){
     Route::post('transcribe', [TranscribeController::class, 'transcribeAudio']);
     Route::get('users', [AuthController::class, 'getUserInfo']);
 });
