8e1af92c9936a6160a770221a10404f075807484
Peter Saba <petersaba001@gmail.com>
2022-11-18 20:49:09 +0200

    frontend <> general: adding dropdown widget

---
+++ frontend/lib/widgets/dropdown.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DropDown extends StatefulWidget {
+  const DropDown({super.key});
+
+  @override
+  State<DropDown> createState() => _DropDownState();
+}
+
+class _DropDownState extends State<DropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
