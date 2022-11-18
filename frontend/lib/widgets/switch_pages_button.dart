535fd7c30522cf955e976ee765b7fc1c2faeebe8
Peter Saba <petersaba001@gmail.com>
2022-11-18 14:13:46 +0200

    frontend <> general: styling second text in switch widget button

--- frontend/lib/widgets/switch_pages_button.dart
+++ frontend/lib/widgets/switch_pages_button.dart
@@ -13,7 +13,7 @@ class SwitchButton extends StatelessWidget {
         ),
         child: Row(
           children: const [Text("Don't have an account? ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 16),),
-          Text('Sign Up')],
+          Text('Sign Up', style: TextStyle(color: Color(0xFF28AFB0), fontSize: 16, fontWeight: FontWeight.bold))],
         ));
   }
 }
