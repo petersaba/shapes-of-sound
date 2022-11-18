f0b39e21807c3825ec7ba7c38f56420f13336301
Peter Saba <petersaba001@gmail.com>
2022-11-18 21:04:55 +0200

    frontend <> general: adding drop down menu

--- frontend/lib/widgets/dropdown.dart
+++ frontend/lib/widgets/dropdown.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class DropDown extends StatefulWidget {
-  const DropDown({super.key});
+  const DropDown({super.key, required this.text});
+  final String text;
 
   @override
   State<DropDown> createState() => _DropDownState();
@@ -11,6 +13,35 @@ class DropDown extends StatefulWidget {
 class _DropDownState extends State<DropDown> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 330,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.text,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                  height: 40,
+                  child: DropdownButton(
+                    items: [
+                      DropdownMenuItem(child: Text('Male'), value: 'male'),
+                      DropdownMenuItem(
+                        child: Text('Female'),
+                        value: 'female',
+                      )
+                    ],
+                    onChanged: ((value) => 10),
+                  )),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
