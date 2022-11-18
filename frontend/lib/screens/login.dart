1323da1a46e6c41881cb2a353d64c0d597fde1b0
Peter Saba <petersaba001@gmail.com>
2022-11-18 13:42:22 +0200

    frontend <> login: adding app logo

--- frontend/lib/screens/login.dart
+++ frontend/lib/screens/login.dart
@@ -12,6 +12,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column(
+      children: [
+        Image.asset(
+          'assets/images/logo.png',
+          width: 260,
+          height: 260,
+        )
+      ],
+    ));
   }
 }
