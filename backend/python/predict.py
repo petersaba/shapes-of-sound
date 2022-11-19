31e4abbf66c19535beaedaab7920c31170137409
Peter Saba <petersaba001@gmail.com>
2022-11-19 23:03:29 +0200

    asr <> prediction: loading weights into new model

--- backend/python/predict.py
+++ backend/python/predict.py
@@ -1,6 +1,8 @@
 import sys
 import utilities
 import tensorflow as tf
+import transformer
+keras = tf.keras
 
 if __name__ == "__main__":
     if sys.argv[1][-4:] != '.wav':
@@ -11,4 +13,9 @@ if __name__ == "__main__":
     
     # the input passed to predict needs to have 3 dimensions
     audio_data = tf.expand_dims(audio_data, 0) # becomes a batch with one sample
-    
+
+    model = transformer.Transformer()
+    loss = keras.losses.CategoricalCrossentropy(label_smoothing=0.1, from_logits=True)
+    optimizer = keras.optimizers.Adam(0) # model will not be trained here, learning rate is useless in that case
+    model.compile(optimizer, loss)
+    model.load_weights('./saved_model/model')
