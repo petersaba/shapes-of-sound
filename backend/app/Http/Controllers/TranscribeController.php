c348ae54513d9c6fc49bc07a4f13f1d512c27c15
Peter Saba <petersaba001@gmail.com>
2022-11-20 01:16:51 +0200

    backend <> transcribe: creating folders to store audio if they do not exist

--- backend/app/Http/Controllers/TranscribeController.php
+++ backend/app/Http/Controllers/TranscribeController.php
@@ -11,18 +11,30 @@ class TranscribeController extends Controller
     function transcribeAudio(Request $request){
         $current_date = date('Y-m-d-H-i-s');
         $user_id = Auth::id();
+
+        $audios_path = '../python/audios/';
+        if (!is_dir($audios_path)){
+            mkdir($audios_path);
+        }
+
+        $audio_folder = $audios_path . $user_id . '/';
+        if (!is_dir($audio_folder)){
+            mkdir($audio_folder);
+        }
+        $audio_path = $audio_folder . $current_date . '.wav';
+
         $base64_encoded_audio = $request->encoded_audio;
         $decoded_audio = base64_decode($base64_encoded_audio);
 
-        file_put_contents('../python/audios/' . $user_id . $current_date . '.wav', $decoded_audio);
+        file_put_contents($audio_path, $decoded_audio);
         $recording = new Recording;
         $recording->user_id = $user_id;
-        $recording->recording_url = '../python/audios/' . $user_id . $current_date . '.wav';
+        $recording->recording_url = $audio_path;
 
-        if($recording->save()){
+        // if($recording->save()){
             return response()->json([
                 'success' => TRUE
             ]);
-        }
+        // }
     }
 }
