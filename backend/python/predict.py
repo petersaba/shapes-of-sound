78474493f53769b3f30399f567009b22d92b0b09
Peter Saba <petersaba001@gmail.com>
2022-11-19 22:47:06 +0200

    asr <> prediction: adding one dimension to the audio data

--- backend/python/predict.py
+++ backend/python/predict.py
@@ -1,5 +1,6 @@
 import sys
 import utilities
+import tensorflow as tf
 
 if __name__ == "__main__":
     if sys.argv[1][-4:] != '.wav':
@@ -7,3 +8,7 @@ if __name__ == "__main__":
 
     audio_path = sys.argv[1]
     audio_data = utilities.readDataFromAudio(audio_path)
+    
+    # the input passed to predict needs to have 3 dimensions
+    audio_data = tf.expand_dims(audio_data, 0) # becomes a batch with one sample
+    
