d730c0d73c8562c941a1be8a2e4116dcbbe5a27d
Peter Saba <petersaba001@gmail.com>
2022-11-19 22:43:58 +0200

    asr <> prediction: reading audio data

--- backend/python/predict.py
+++ backend/python/predict.py
@@ -1,7 +1,9 @@
 import sys
+import utilities
 
 if __name__ == "__main__":
     if sys.argv[1][-4:] != '.wav':
         exit()
-        
+
     audio_path = sys.argv[1]
+    audio_data = utilities.readDataFromAudio(audio_path)
