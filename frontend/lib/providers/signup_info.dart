09f67158f208045b1f8b88a768d083d63750e9c3
Peter Saba <petersaba001@gmail.com>
2022-11-20 20:22:03 +0200

    frontend <> general: adding getters and setters for sign up info provider

--- frontend/lib/providers/signup_info.dart
+++ frontend/lib/providers/signup_info.dart
@@ -1,5 +1,54 @@
 import 'package:flutter/cupertino.dart';
 
 class SignUpInfo with ChangeNotifier {
-  
+  String? _email;
+  String? _fullName;
+  String? _password;
+  String? _confPassword;
+  String? _gender;
+
+  String? getAttribute(String attribute) {
+    switch (attribute) {
+      case 'email':
+        return _email;
+
+      case 'full_name':
+        return _fullName;
+
+      case 'password':
+        return _password;
+
+      case 'confPassword':
+        return _confPassword;
+
+      case 'gender':
+        return _gender;
+    }
+
+    return null;
+  }
+
+  void setAttribute(String attribute, String value) {
+    switch (attribute) {
+      case 'email':
+        _email = value;
+        return;
+
+      case 'full_name':
+        _fullName = value;
+        return;
+
+      case 'password':
+        _password = value;
+        return;
+
+      case 'confPassword':
+        _confPassword = value;
+        return;
+
+      case 'gender':
+        _gender = value;
+        return;
+    }
+  }
 }
