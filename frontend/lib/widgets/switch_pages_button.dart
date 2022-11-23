2d8fb3f65963c1f289cb420acc20c102ef1f85ef
Peter Saba <petersaba001@gmail.com>
2022-11-23 15:37:15 +0200

    frontend <> general: adding paramters to show button text

--- frontend/lib/widgets/switch_pages_button.dart
+++ frontend/lib/widgets/switch_pages_button.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class SwitchButton extends StatelessWidget {
-  const SwitchButton({super.key, required this.route});
+  const SwitchButton(
+      {super.key, required this.route, required this.blackText, required this.coloredText});
+  final String blackText;
+  final String coloredText;
   final String route;
 
   @override
@@ -15,16 +18,16 @@ class SwitchButton extends StatelessWidget {
                 shadowColor: Colors.transparent),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
+              children: [
                 Text(
-                  "Don't have an account? ",
-                  style: TextStyle(
+                  "$blackText ",
+                  style: const TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.w500,
                       fontSize: 16),
                 ),
-                Text('Sign Up',
-                    style: TextStyle(
+                Text( coloredText,
+                    style: const TextStyle(
                         color: Color(0xFF28AFB0),
                         fontSize: 16,
                         fontWeight: FontWeight.bold))
